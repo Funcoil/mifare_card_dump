fe16b838a79232293398c79ba00c6a4c18385572
Martin Habovštiak <martin.habovstiak@gmail.com>
2016-11-26 12:37:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Mifare card dumper
+==================
+
+This simple program dumps mifare cards. Using PN532 connected with i2c with busy waiting.
+
+It tries to use default key for all sectors.
+
+To use it, just run it and hold the card within antenna range.
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+extern crate pn532;
+extern crate mifare;
+
+use pn532::tags::{ISO14443AListOptions};
+use mifare::MifareTag;
+
+fn main() {
+    let i2c = pn532::bus::i2c::open("/dev/i2c-0").unwrap();
+    let mut device = pn532::PN532::new(pn532::bus::BusyWait::new(i2c));
+    device.sam_configure(pn532::SAMMode::Normal(None)).unwrap();
+    let list_opts = ISO14443AListOptions {
+        limit: pn532::tags::TagNumLimit::One,
+        uid: None
+    };
+    let mut tag_buf = pn532::tags::TagBuffer::new();
+    let tags = device.list_tags(list_opts, &mut tag_buf).unwrap();
+    let tag = tags.first();
+    let mut tag = MifareTag::new(tag).unwrap();
+
+    let mut rbuf = [0u8; 16];
+    for sector_number in 0..16 {
+		println!("-------------------Sector {:02}-------------------", sector_number);
+        let mut sector = tag.authenticate_sector(sector_number, &[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]).unwrap();
+        for block in 0..4 {
+            sector.read_block(block, &mut rbuf).unwrap();
+            for b in &rbuf {
+                print!("{:02X} ", *b);
+            }
+            println!("");
+        }
+    }
+}
